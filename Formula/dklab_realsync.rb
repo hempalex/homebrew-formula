17855d6f6074b360ec5a1e79bb92581d9d39d4d4
Alexsander Mikhailov <support@indico.ru>
2024-08-29 23:29:08 +0300

    Skip livecheck for realsync

--- Formula/dklab_realsync.rb
+++ Formula/dklab_realsync.rb
@@ -1,10 +1,13 @@
-require "formula"
-
 class DklabRealsync < Formula
   url "https://github.com/DmitryKoterov/dklab_realsync/archive/master.tar.gz"
   homepage "https://github.com/DmitryKoterov/dklab_realsync"
   version "1.0"
   sha256 "57efa311ce9a6f996c5e4853fa4b495804cf65ca5a853a12a546024e9ee5115a"
+
+  livecheck do
+    skip "No version information available"
+  end
+
 
   def install
     inreplace "realsync", "bin/darwin/notify", "notify"
