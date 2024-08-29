9427614624c79ef7b7ee6644a15e8b8558602232
Alexsander Mikhailov <support@indico.ru>
2024-08-29 23:13:32 +0300

    SpoofDPI App cask

---
+++ Casks/spoofdpi-app.rb
@@ -0,0 +1,15 @@
+cask "spoofdpi-app" do
+  version "1.2.1"
+  sha256 "11d6b93420093ed7476ff2d93502da8eab4b852e741a0e0ecda013fb537122cc"
+  url "https://github.com/SpoofDPIApp/SpoofDPI-App/releases/download/#{version}/SpoofDPI.App.zip"
+  name "SpoofDPI-App"
+  desc "spoofdpi frontend for macos"
+  homepage "https://github.com/SpoofDPIApp/SpoofDPI-App"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "SpoofDPI App.app"
+end
